b3a3007077c63fcf79a86ad2c6704f918d9c8338
lunaisnotaboy <her@mint.lgbt>
2021-01-09 19:55:17 -0500

    about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'about/index'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'home/index'
